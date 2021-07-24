fa5f7baa08df83523afa05275019616c5eee19d5
yoon-seul <66136653+yoon-seul@users.noreply.github.com>
2021-07-24 17:24:16 +0900

    Add files via upload

--- 15649_backtracking.c
+++ 15649_backtracking.c
@@ -1,27 +1,40 @@
-//BaekJoon #15649 by yoonseul at 210715
+//BaekJoon #15649 by yoonseul at 210724
 
 #include <stdio.h>
+#include <stdbool.h>
 
 void series(int x, int y);
+int result[8];
 
 int main(void)
 {
-	int inputA, inputB;
+	int n, m;
 
-	scanf("%d %d", &inputA, &inputB);
+	scanf("%d %d", &n, &m);
 
-	series(inputA, inputB);
+	series(n, m,0);
 }
 
-void series(int x, int y)
+void series(int x, int y, int depth)
 {
-	int num[8];
-	
-	for (int i = 0; i < x; i++) {
-		num[i] = i + 1;
-	}
 
-	for (int i = 0; i < y; i++) {
-		printf("%d ",)
+	if (depth == 0) {
+		for (int i = 0; i < x; i++) {
+			result[depth] = i + 1;
+			series(x, y - 1, depth + 1);
+		}
 	}
+	else {
+		for (int i = 0; i < x; i++) {
+			if (result[depth - 1] == i + 1)
+				continue;
+			else {
+				result[depth] = i + 1;
+			}
+		}
+		series(x, y - 1, depth + 1);
+	}
+	if (y == depth)
+		exit(1);
+	
 }
