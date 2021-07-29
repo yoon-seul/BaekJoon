e35b792e08122a097ea2d83f5be7146433328f97
yoon-seul <66136653+yoon-seul@users.noreply.github.com>
2021-07-29 22:38:07 +0900

    Add files via upload

--- 15650_backtracking.c
+++ 15650_backtracking.c
@@ -1,23 +1,29 @@
-//BaekJoon #15650 by yoonseul at 210724~210725
+/*BaekJoon #15650 by yoonseul at 210724~210725, 210729*/
 
 #include <stdio.h>
 
 void series(int* arr[], int x, int y, int depth)
 {
-	for (int i = 0; i <= x; i++) {
+	for (int i = 0; i < x; i++) {
 		int j;
 		if (depth == 0) 
 			arr[0] = i+1;
 		for (j = 1; j < y+1; j++) {
-			if (arr[j] <= i+1)
+			if (arr[j-1] >= i+1)
 				break;
 			else{
 				arr[j] = i+1;
 			}
 			
 		}
-		if (j == depth)
-			printf("%d ", arr[j]);
+		if (y == depth) {
+			for (int k = 0; k < y; k++) {
+				printf("%d ", arr[k]);
+				printf("\n");
+			}
+
+			return 0;
+		}
 		series(&arr, x, y, depth + 1);
 	}
 }
