c88664fe313d43a9e67bba8b962cf7f63543eb7d
yoon-seul <66136653+yoon-seul@users.noreply.github.com>
2021-07-30 10:56:58 +0900

    Add files via upload

--- 15650_backtracking.c
+++ 15650_backtracking.c
@@ -1,38 +1,44 @@
-/*BaekJoon #15650 by yoonseul at 210724~210725, 210729*/
+/*BaekJoon #15650 by yoonseul at 210724~210725, 210729~210730*/
 
 #include <stdio.h>
 
-int n, m;
-int result[8] = { 0 };
+
+
 
 void series(int arr[], int x, int y, int depth)
 {
-	for (int i = 0; i < x; i++) {
+	for (int i = 1; i < x+1; i++) {
 		int j;
-		if (depth == 0) 
-			arr[0] = i+1;
-		for (j = 1; j < y+1; j++) {
-			if (arr[j-1] >= i+1)
-				break;
-			else{
-				arr[j] = i+1;
+		if (depth == 0)
+			arr[0] = i;
+		else {
+			for (j = 0; j < depth; j++) {
+				if (arr[j] >= i)
+					break;
 			}
-			
+			if (j == depth)
+					arr[j] = i;
+				else {
+					continue;
+				}
 		}
-		if (y == depth) {
-			for (int k = 0; k < y; k++) {
-				printf("%d ", arr[k]);
+		
+		if (y-1 == depth) {
+			for (int i = 0; i < y; i++) {
+				printf("%d ", arr[i]);
 			}
 			printf("\n");
-			return 0;
 		}
-		series(arr, x, y, depth + 1);
+		else {
+			series(arr, x, y, depth + 1);
+		}
 	}
 }
 
 int main(void)
 {
-	
+	int n, m;
+	int result[8] = { 0, };
 
 	scanf("%d %d", &n, &m);
 
