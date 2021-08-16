a569f2c4d908bdddf40ddbfa84d3ad69546e0eed
yoon-seul <66136653+yoon-seul@users.noreply.github.com>
2021-08-16 21:38:30 +0900

    Add files via upload

--- 7568.c
+++ 7568.c
@@ -5,18 +5,41 @@
 typedef struct howLarge {
 	int weight;
 	int height;
+	int seriesW;
+	int seriesH;
+	int series;
 }HL;
 
 int main(void)
 {
 	int num;
 	struct HL;
+	HL person[50];
 
 	scanf("%d", &num);
 
-	HL person[50];
-
 	for (int i = 0; i < num; i++) {
 		scanf("%d %d", person[i].weight, person[i].height);
+	}
+
+	for (int i = 0; i < num; i++) {
+		int count = 0;
+		for (int j = 0; j < num; j++) {
+			if (person[i].weight < person[j].weight)
+				count++;
+		}
+		person[i].seriesW = count;
+	}
+	for (int i = 0; i < num; i++) {
+		int count = 0;
+		for (int j = 0; j < num; j++) {
+			if (person[i].height < person[j].height)
+				count++;
+		}
+		person[i].seriesH = count;
+	}
+
+	for (int i = 0; i < num; i++) {
+		if
 	}
 }
